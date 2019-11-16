e758422029d95276cb789c7c913674b934b00611
Richard Oosterlaken <richard.oosterlaken@gmail.com>
2019-11-16 15:35:35 +0100

    Add Map component.

--- frontend/src/modules/Map/Map.js
+++ frontend/src/modules/Map/Map.js
@@ -1,12 +1,23 @@
 import React from "react";
-import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+import styled from "styled-components";
+import ReactMapboxGl from "react-mapbox-gl";
 
 const Map = ReactMapboxGl({
   accessToken: process.env.REACT_APP_MAPBOX_ACCESS_TOKEN
 });
 
-const MapContainer = styled.div``;
+const MapWrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+`;
 
-export default props => {
-  return <MapContainer></MapContainer>;
-};
+export default props => (
+  <MapWrapper>
+    <Map
+      style="mapbox://styles/xheory/ck31eegwo0mfp1co4kxr8nmd8?fresh=true"
+      containerStyle={{ height: "100%", width: "100%" }}
+      center={[24.2957, 60.1827]}
+    ></Map>
+  </MapWrapper>
+);
